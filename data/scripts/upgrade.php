741b0e078e21dca39c6ca042391db970b0b960dc
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-01-09 00:00:00 +0000

    Cleaned code.

--- data/scripts/upgrade.php
+++ data/scripts/upgrade.php
@@ -2,27 +2,27 @@
 
 namespace Menu;
 
-use Omeka\Mvc\Controller\Plugin\Messenger;
 use Omeka\Stdlib\Message;
 
 /**
  * @var Module $this
- * @var \Laminas\ServiceManager\ServiceLocatorInterface $serviceLocator
+ * @var \Laminas\ServiceManager\ServiceLocatorInterface $services
  * @var string $newVersion
  * @var string $oldVersion
  *
- * @var \Doctrine\DBAL\Connection $connection
- * @var \Doctrine\ORM\EntityManager $entityManager
  * @var \Omeka\Api\Manager $api
  * @var \Omeka\Settings\Settings $settings
- * @var \Omeka\Settings\SiteSettings $siteSettings
+ * @var \Doctrine\DBAL\Connection $connection
+ * @var \Doctrine\ORM\EntityManager $entityManager
+ * @var \Omeka\Mvc\Controller\Plugin\Messenger $messenger
  */
-$services = $serviceLocator;
-$connection = $services->get('Omeka\Connection');
-$entityManager = $services->get('Omeka\EntityManager');
 $plugins = $services->get('ControllerPluginManager');
 $api = $plugins->get('api');
 $settings = $services->get('Omeka\Settings');
+$connection = $services->get('Omeka\Connection');
+$messenger = $plugins->get('messenger');
+$entityManager = $services->get('Omeka\EntityManager');
+
 $siteSettings = $services->get('Omeka\Settings\Site');
 
 if (version_compare($oldVersion, '3.3.1.1', '<')) {
@@ -42,7 +42,6 @@ SQL;
         $message = new Message(
             'The settings for "Breadcrumbs" were upgraded.' // @translate
         );
-        $messenger = new Messenger();
         $messenger->addWarning($message);
     }
 }
@@ -79,7 +78,6 @@ if (version_compare($oldVersion, '3.3.5', '<')) {
     $settings->set('menu_property_itemset', $settings->get('next_property_itemset', ''));
 
     $urlHelper = $services->get('ViewHelperManager')->get('url');
-    $messenger = new Messenger();
     $message = new Message(
         'The helper "PrimaryItemSet" was moved from module %1$sNext%2$s and a param is added for it in %3$smain settings%2$s.', // @translate
         '<a href="https://gitlab.com/Daniel-KM/Omeka-S-module-Next" target="_blank">',
